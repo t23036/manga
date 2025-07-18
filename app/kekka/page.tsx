3135a0fe4ab1d294e372f41dd28ebca71c511f10
T23036 <t23036tt@aitech.ac.jp>
2025-07-18 11:12:23 +0900

    Update page.tsx

--- app/kekka/page.tsx
+++ app/kekka/page.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import DropdownItem from "../components4/DropdownItem ";
-import ImageTextRow from "../components4/ImageTestRow";
-
 export default function KekkaPage(){
     return (
      <div>
       <h1>検索</h1>
-      <ImageTextRow />
-      <DropdownItem />
      </div>
     );
 }
