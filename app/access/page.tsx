7e440a0912c5f6061dfa89aecdfb74835ddb9f6e
T23036 <t23036tt@aitech.ac.jp>
2025-07-25 12:01:13 +0900

    Update page.tsx

--- app/access/page.tsx
+++ app/access/page.tsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Result from "../kensaku2/page";
 
 export default function AccessPage(){
     return (
      <div>
+    <Suspense fallback={<p>Loading...</p>}>
       <Result />
+    </Suspense>
      </div>
     );
 }
